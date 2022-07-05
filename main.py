da9acbd5472c34a9f9a383c84974e8222e9c71d2
tedd2k <aaliff70@gmail.com>
2022-07-05 04:54:37 +0000

    updated client code

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import socket
 size_of_board = 1200
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
 symbol_thickness = 10
-symbol_X_color = '#EE4035'
-symbol_O_color = '#0492CF'
-Green_color = '#7BC043'
+symbol_X_color = '#FF69B4'
+symbol_O_color = '#0047AB'
+Green_color = '#355E3B'
 
 s = socket.socket()
 
